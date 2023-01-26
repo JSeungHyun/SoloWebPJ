15a5c4ea3c0b3365763bfd19350bc1b30e28b126
JSeungHyun <net1645@naver.com>
2023-01-26 21:02:41 +0900

    start

---
+++ src/main/java/com/example/solowebproject/SoloWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.solowebproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloWebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoloWebProjectApplication.class, args);
+    }
+
+}
